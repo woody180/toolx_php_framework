1af03a240fc05dcde50985dcb9793ffd59649840
woody180 <woody180@rocketmail.com>
2023-06-25 22:55:07 +0100

    remove classes

--- app/Views/welcome.php
+++ app/Views/welcome.php
@@ -1,8 +1,8 @@
 <?= $this->layout('partials/template', ['title' => $title]) ?>
 
 <?= $this->start('mainSection') ?>
-<section class="uk-section">
-    <div class="uk-container uk-container-small">
+<section>
+    <div>
 
         <h1><?= $title ?></h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo asperiores beatae officia accusamus quibusdam nobis ipsum repudiandae. Corporis odit voluptatem eveniet modi unde, nam fuga blanditiis harum, ut delectus optio.</p>    
