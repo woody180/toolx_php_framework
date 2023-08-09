0dd58a78dc8680dd8c0e32580f68c9dfc6baf341
woody180 <woody180@rocketmail.com>
2023-08-09 08:35:35 +0400

    users route path in routes config

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -5,4 +5,6 @@
     * ['Back', 'Front']
     * Rotues will be search in app\Routes\Back and app\Routes\Front - automatically.
 */  
-define('ROUTES_PATH', []);
+define('ROUTES_PATH', [
+    //'users'
+]);
