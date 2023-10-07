17b733a9c851fb9907439e1edd1e27453500b281
woody180 <35693431+woody180@users.noreply.github.com>
2023-10-07 14:47:38 +0400

    Update scssCompiler.php
    Create scss file written in scss configuration

--- app/Boot/scssCompiler.php
+++ app/Boot/scssCompiler.php
@@ -3,6 +3,9 @@
 if (ENV === 'development' && COMPILE === TRUE)
 {
     $compiler = new ScssPhp\ScssPhp\Compiler();
+
+    // Check if from file exists
+    if (!file_exists(COMPILE_FROM . '/' . COMPILATIONS_FILE_FROM)) file_put_contents(COMPILE_FROM . '/' . COMPILATIONS_FILE_FROM, '');
     
     $compiler->setImportPaths(COMPILE_FROM);
     $scss = file_get_contents(COMPILE_FROM . '/' . COMPILATIONS_FILE_FROM);
