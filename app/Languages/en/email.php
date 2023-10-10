65eee29fb630a5556310841cae85ceab7657356c
woody180 <woody180@rocketmail.com>
2023-10-10 11:36:45 +0400

    remove lang/ge & added lang/en

--- app/Languages/en/email.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    "reset_post_message" => "To reset password follow the link",
-    "reset_verification_message" => "A verification link has been sent to the email you provided. Passwords will be reset after following the sent link.",
-    "wrong_verification_code" => "Wrong verification code.",
-    "password_change_success" => "Password has been changed successfully."
-];
