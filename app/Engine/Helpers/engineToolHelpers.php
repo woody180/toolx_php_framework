ed4df60a5fa13d37e525523366564c8311a43175
woody180 <woody180@rocketmail.com>
2023-11-02 11:13:05 +0400

    remove directory

--- app/Engine/Helpers/engineToolHelpers.php
+++ app/Engine/Helpers/engineToolHelpers.php
@@ -199,6 +199,23 @@ function rrmdir($dir) {
 }
 
 
+function rmdir($dir) { 
+    if (is_dir($dir)) { 
+        $objects = scandir($dir);
+        foreach ($objects as $object) { 
+            if ($object != "." && $object != "..") { 
+            if (is_dir($dir. DIRECTORY_SEPARATOR .$object) && !is_link($dir."/".$object))
+                rrmdir($dir. DIRECTORY_SEPARATOR .$object);
+            else
+                unlink($dir. DIRECTORY_SEPARATOR .$object); 
+            } 
+        }
+        rmdir($dir); 
+    } 
+}
+
+
+
 
 function isJSON($string){
     return is_string($string) && is_array(json_decode($string, true)) ? true : false;
