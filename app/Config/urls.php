ab6b022a8341387e29ad228ba438c5d39ee75b76
woody180 <woody180@rocketmail.com>
2024-12-10 13:06:17 +0400

    current_url fix

--- app/Config/urls.php
+++ app/Config/urls.php
@@ -5,4 +5,4 @@ define("URLROOT", "http://localhost:9191");
 //define("URLROOT", (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]");
 define("PUBLIC_DIR", URLROOT . "/assets");
 define("APPROOT", dirname(dirname(__FILE__)));
-define("CURRENT_URL", URLROOT . $_SERVER['REQUEST_URI']);
+define("CURRENT_URL", URLROOT . ($_SERVER['REQUEST_URI'] ?? ''));
