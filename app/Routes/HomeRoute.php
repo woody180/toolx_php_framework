960e991883a5a1107ecae6f11c0eecabb3dcd946
woody180 <woody180@rocketmail.com>
2025-01-30 12:43:51 +0400

    Update home route

--- app/Routes/HomeRoute.php
+++ app/Routes/HomeRoute.php
@@ -4,6 +4,9 @@ use App\Engine\Libraries\Router;
 
 $router = Router::getInstance();
 
-$router->get('snow-fall, like-snow, snowfall, snow, fall', 'HomeController@index');
+$router->get('/', 'HomeController@index');
+$router->get('snow-fall, like-snow, snowfall, snow, fall', function($req, $res) {
+    echo "Snow is falling";
+});
 $router->get('page/gallery, page/gallery-page', 'HomeController@gallery');
 $router->get('page/(:segment)/(:num)', 'HomeController@about');
