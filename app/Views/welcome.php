82a18126f68f188a7e3a2a3c892e8f7693d7c216
woody180 <woody180@rocketmail.com>
2025-10-18 14:10:11 +0400

    update welcome page

--- app/Views/welcome.php
+++ app/Views/welcome.php
@@ -7,7 +7,7 @@
         <h1><?= $title ?></h1>
         <p>Go to page, <a href="<?= url_to('HomeController@about', 'about', 1) ?>">About page</a>.</p>    
         <p>Or take a look at <a href="<?= url_to('HomeController@gallery') ?>">Gallery page</a>.</p>
-        <a href="<?= baseUrl('file-manager') ?>">Open editor page</a>
+        <a href="<?= baseUrl('file-manager') ?>">Open editor page</a> OR <a href="#" onclick="window.renderFileManager(); return false;">Open file manager</a>
             
     </div>
 </section>
