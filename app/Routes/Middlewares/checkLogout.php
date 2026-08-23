d70d96a0b62cc1fc280f58d4f330d94ae89f2c78
woody180 <woody180@rocketmail.com>
2026-08-23 12:10:07 +0400

    check logout middleware accourding to guid

--- app/Routes/Middlewares/checkLogout.php
+++ app/Routes/Middlewares/checkLogout.php
@@ -2,5 +2,5 @@
 
 function checkLogout() {
     if (isset($_SESSION['userid']))
-        return header('Location: ' . baseUrl('users/profile/' . $_SESSION['userid']));
+        return header('Location: ' . baseUrl('users/profile/' . initModel('users')->getUser($_SESSION['userid'])->guid));
 }
