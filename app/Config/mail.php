af42e5c32458c04edddaea99916c8c3f2dc1bbd0
woody180 <woody180@rocketmail.com>
2026-08-23 12:32:58 +0400

    add dev mail variables

--- app/Config/mail.php
+++ app/Config/mail.php
@@ -1,8 +1,17 @@
 <?php
 
+// PRODUCTION
 define("AUTH_DOMAIN", "your_domain_name");
 define("AUTH_MAIL", "one@one.com");
 define("MAIL_HOST", "");
 define("MAIL_USERNAME", "");
 define("MAIL_PASSWORD", "");
 define("MAIL_PORT", 587);
+
+// DEVELOPMENT
+define("DEV_AUTH_DOMAIN", "");
+define("DEV_AUTH_MAIL", "");
+define("DEV_MAIL_HOST", "localhost");
+define("DEV_MAIL_USERNAME", "");
+define("DEV_MAIL_PASSWORD", "");
+define("DEV_MAIL_PORT", 8025);
